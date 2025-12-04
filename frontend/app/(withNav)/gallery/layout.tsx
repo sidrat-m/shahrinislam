8ff5cd17a11528d52d8f14b2957ee0b0c27cfc2b
sidrat-m <sidratmarzan@gmail.com>
2025-12-04 11:57:41 +0600

    Update layout.tsx

--- frontend/app/(withNav)/gallery/layout.tsx
+++ frontend/app/(withNav)/gallery/layout.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Gallery | Hummam Quader Chowdhury",
-  description: "Learn about Hummam Quader Chowdhury, his background, and his vision for Rangunia.",
+  title: "Gallery | Shahrin Islam Tuhin",
+  description: "Learn about Shahrin Islam Tuhin, his background, and his vision for Nilphamari.",
 }
 
 export default function GalleryLayout({
